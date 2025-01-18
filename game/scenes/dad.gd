6673485d3fc51bf2b0d610a4a54bd2b9ce481199
FrayWilly94 <fcrocombe@gmail.com>
2025-01-19 07:48:04 +1300

    Made "Dad" characterbody 2d. Added rotational movement

--- game/scenes/dad.gd
+++ game/scenes/dad.gd
@@ -18,12 +18,16 @@ func _process(delta: float) -> void:
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed(up):
 		velocity.y -= 1
+		rotation_degrees = -90
 	if Input.is_action_pressed(down):
 		velocity.y += 1
+		rotation_degrees = 90
 	if Input.is_action_pressed(left):
 		velocity.x -= 1
+		rotation_degrees = -180
 	if Input.is_action_pressed(right):
 		velocity.x += 1
+		rotation_degrees = 0
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
