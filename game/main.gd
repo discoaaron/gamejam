f590da058de86c3e37c85166277796519d183db5
Aaron Small <discoaaron2@gmail.com>
2025-01-18 23:18:10 +1300

    adds randomising of player controls

--- game/main.gd
+++ game/main.gd
@@ -1,9 +1,25 @@
 extends Node2D
 
+@export var dad_scene: PackedScene
+
+var keys = ["w", "a", "s", "d" ]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
+	var dad = dad_scene.instantiate();
+	dad.position = Vector2(150,120)
+	dad.up = get_button()
+	dad.down = get_button()
+	dad.left = get_button()
+	dad.right = get_button()
+	add_child(dad)
+	
+func get_button() -> String:
+	var key;
+	var keys_index = randi_range(0, keys.size() - 1);
+	key = keys[keys_index]
+	keys.remove_at(keys_index)
+	return key;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
