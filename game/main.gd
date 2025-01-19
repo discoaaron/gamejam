c197bc066032c81f7cd37f4ec963a741cfb062c5
Aaron Small <discoaaron2@gmail.com>
2025-01-19 15:59:46 +1300

    dynamically add baby to scene

--- game/main.gd
+++ game/main.gd
@@ -1,20 +1,17 @@
 extends Node2D
 
-@export var dad_scene: PackedScene
+var dad_scene = preload("res://scenes/dad/dad.tscn")
+var baby_scene = preload("res://scenes/baby/baby.tscn")
 
-
+var screen_size # Size of the game window.
 
 var keys = ["w", "a", "s", "d" ]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var dad = dad_scene.instantiate();
-	dad.position = Vector2(200,200)
-	dad.up = get_button()
-	dad.down = get_button()
-	dad.left = get_button()
-	dad.right = get_button()
-	add_child(dad)
+	screen_size = get_viewport_rect().size
+	_spawnDad()
+	_spawnBaby()
 	
 func get_button() -> String:
 	var key;
@@ -26,3 +23,20 @@ func get_button() -> String:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _spawnDad() -> void:
+	var dad = dad_scene.instantiate();
+	dad.position = _getRandomPositionOnScreen()
+	dad.up = get_button()
+	dad.down = get_button()
+	dad.left = get_button()
+	dad.right = get_button()
+	add_child(dad)
+	
+func _spawnBaby() -> void:
+	var baby = baby_scene.instantiate()
+	baby.position = _getRandomPositionOnScreen()
+	add_child(baby)
+	
+func _getRandomPositionOnScreen() -> Vector2:
+	return Vector2(randi_range(0, screen_size.x),randi_range(0, screen_size.y))
