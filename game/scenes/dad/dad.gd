f3fa27d29b6a33d2efdeff2fafa874253971cef2
Aaron Small <discoaaron2@gmail.com>
2025-01-20 21:35:36 +1300

    remove the old dad (code)

--- game/scenes/dad/dad.gd
+++ game/scenes/dad/dad.gd
@@ -21,24 +21,9 @@ func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	SignalManager.baby_enter.connect(on_baby_area2d)
 	SignalManager.baby_exit.connect(on_baby_exit)
-	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#var velocity = Vector2.ZERO # The player's movement vector.
-	#if Input.is_action_pressed(up):
-		#velocity.y -= 1
-		#rotation_degrees = -90
-	#if Input.is_action_pressed(down):
-		#velocity.y += 1
-		#rotation_degrees = 90
-	#if Input.is_action_pressed(left):
-		#velocity.x -= 1
-		#rotation_degrees = -180
-	#if Input.is_action_pressed(right):
-		#velocity.x += 1
-		#rotation_degrees = 0
 	if Input.is_action_pressed("q"):
 		fire_laser(self.position, Vector2.RIGHT)
 	if Input.is_action_just_pressed(action):
@@ -49,17 +34,6 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("z"):
 		#dashing = true
 		dash_action()
-	
-	#if velocity.length() > 0:
-		#velocity = velocity.normalized() * speed
-		#$AnimatedSprite2D.play()
-	#else:
-		#$AnimatedSprite2D.stop()
-		
-	#position += velocity * delta
-	#position = position.clamp(Vector2.ZERO, screen_size)
-	
-
 
 func _physics_process(delta: float) -> void:	
 	if not dashing:
