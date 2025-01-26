d88136e1c4fba666d356a3941fc2e3f7e8f52b96
FrayWilly94 <fcrocombe@gmail.com>
2025-01-26 19:34:33 +1300

    Added risk group - fixed laser bug

--- game/scenes/baby/baby.gd
+++ game/scenes/baby/baby.gd
@@ -2,17 +2,6 @@ extends Node2D
 
 class_name Baby
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-
 
 func _on_body_area_entered(area: Area2D) -> void:
 	SignalManager.baby_enter.emit(area)
--- game/scenes/laser/laser.gd
+++ game/scenes/laser/laser.gd
@@ -6,7 +6,7 @@ var based_width = 10
 var widthy = based_width
 var shoot = false
 var active = false
-var things_to_check = ["Toaster", "Baby"]
+var risk_group = ["RiskItems"]
 @export var laser_direction: Vector2 = Vector2.RIGHT  # Direction the laser fires
 @onready var collision = $Line2D/DamageArea/CollisionShape2D
 
@@ -24,9 +24,10 @@ func _process(delta):
 		if is_colliding():
 			$Line2D.points[1] = to_local(get_collision_point())
 			var collidedThing = get_collider()
-			if collidedThing is Baby or Toaster or Toilet:
-				SignalManager.risk_item_lasered.emit(collidedThing)
-				#print("Collided with: ", collidedThing)
+			for group in risk_group:
+				if collidedThing.is_in_group(group):
+					SignalManager.risk_item_lasered.emit(collidedThing)
+					#print("Collided with: ", collidedThing)
 		else:
 			$Line2D.points[1] = target_position
 	else:
